c4c563c914d5fcb6f6a605797a38d981c0e00351
chouvel <2021b33095@stu.zjweu.edu.cn>
2024-11-02 20:03:31 +0800

    update

--- sw.js
+++ sw.js
@@ -21,6 +21,7 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   "./img/icon_wechat.png",
   "./img/avatar-hux.jpg",
+  "./img/avatar.png",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
